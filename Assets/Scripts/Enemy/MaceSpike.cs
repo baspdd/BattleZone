8b42280e322ca26a59daa9556d260c968f0c57a5
baspdd <duypdhe160308@fpt.edu.vn>
2023-07-13 23:00:38 +0700

    update

--- Assets/Scripts/Enemy/MaceSpike.cs
+++ Assets/Scripts/Enemy/MaceSpike.cs
@@ -1,9 +1,11 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MaceSpike : MonoBehaviour
 {
+    [SerializeField] private GameObject enemyDeath;
     [SerializeField] float amplitude = 2;
     Vector3 startPosition;
     // Start is called before the first frame update
@@ -16,5 +18,17 @@ public class MaceSpike : MonoBehaviour
     void Update()
     {
         transform.position = startPosition + amplitude * new Vector3(0f, Mathf.Sin(Time.time), 0);
+    }
+
+    private async void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer is (int)Layer.Fire)
+        {
+            var death = Instantiate(this.enemyDeath);
+            death.transform.localPosition = transform.position;
+            Destroy(this.gameObject);
+            await UniTask.Delay(500);
+            Destroy(death);
+        }
     }
 }
